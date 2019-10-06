88796d1885b884279123e20fe84d437f544bb08e
chakravarthy <ballachakri@yahoo.com>
2019-10-06 13:06:56 +0100

    configuring jenkins

--- src/test/reports/cucumberReport.html/report.js
+++ src/test/reports/cucumberReport.html/report.js
@@ -59,3 +59,204 @@ formatter.background({
   "description": "",
   "keyword": "Background"
 });
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I am on the home page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_am_on_the_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User should be able to search a product",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@singlesearch"
+    }
+  ]
+});
+formatter.step({
+  "name": "I search for a product \"shirt\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_search_for_a_product(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "i should be able to view all the relevant products",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_should_be_able_to_view_all_the_relevant_products()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I am on the home page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_am_on_the_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User should be able to search a product",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@singlesearch"
+    }
+  ]
+});
+formatter.step({
+  "name": "I search for a product \"bag\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_search_for_a_product(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "i should be able to view all the relevant products",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_should_be_able_to_view_all_the_relevant_products()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I am on the home page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_am_on_the_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User should be able to search a product",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@singlesearch"
+    }
+  ]
+});
+formatter.step({
+  "name": "I search for a product \"skirt\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_search_for_a_product(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "i should be able to view all the relevant products",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_should_be_able_to_view_all_the_relevant_products()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I am on the home page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_am_on_the_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User should be able to search a product",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@singlesearch"
+    }
+  ]
+});
+formatter.step({
+  "name": "I search for a product \"shoe\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_search_for_a_product(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "i should be able to view all the relevant products",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ShouldBeAbleToSearchProductStepDefs.i_should_be_able_to_view_all_the_relevant_products()"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.TimeoutException: Expected condition failed: waiting for visibility of Proxy element for: DefaultElementLocator \u0027By.cssSelector: p[class\u003d\u0027page_summary\u0027] span\u0027 (tried for 20 second(s) with 500 milliseconds interval)\r\n\tat org.openqa.selenium.support.ui.WebDriverWait.timeoutException(WebDriverWait.java:95)\r\n\tat org.openqa.selenium.support.ui.FluentWait.until(FluentWait.java:272)\r\n\tat utils.WaitUntil.elementToBeVisible(WaitUntil.java:20)\r\n\tat PageObjects.HomePage.HomePage.getSearchResultTitle(HomePage.java:39)\r\n\tat stepdefinitions.ShouldBeAbleToSearchProductStepDefs.i_should_be_able_to_view_all_the_relevant_products(ShouldBeAbleToSearchProductStepDefs.java:33)\r\n\tat ✽.i should be able to view all the relevant products(file:src/test/resources/ShouldBeAbleToSearchProduct.feature:13)\r\nCaused by: org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"p[class\u003d\u0027page_summary\u0027] span\"}\n  (Session info: chrome\u003d77.0.3865.90)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027CHAKRA-PC\u0027, ip: \u0027192.168.56.1\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_221\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 77.0.3865.90, chrome: {chromedriverVersion: 77.0.3865.40 (f484704e052e0..., userDataDir: C:\\Users\\sball\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:50574}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: XP, platformName: XP, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: b2c9352d805cf96324e57d230dc3a961\n*** Element info: {Using\u003dcss selector, value\u003dp[class\u003d\u0027page_summary\u0027] span}\r\n\tat sun.reflect.GeneratedConstructorAccessor14.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByCssSelector(RemoteWebDriver.java:420)\r\n\tat org.openqa.selenium.By$ByCssSelector.findElement(By.java:431)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy17.isDisplayed(Unknown Source)\r\n\tat org.openqa.selenium.support.ui.ExpectedConditions.elementIfVisible(ExpectedConditions.java:314)\r\n\tat org.openqa.selenium.support.ui.ExpectedConditions.access$000(ExpectedConditions.java:43)\r\n\tat org.openqa.selenium.support.ui.ExpectedConditions$10.apply(ExpectedConditions.java:300)\r\n\tat org.openqa.selenium.support.ui.ExpectedConditions$10.apply(ExpectedConditions.java:297)\r\n\tat org.openqa.selenium.support.ui.FluentWait.until(FluentWait.java:249)\r\n\tat utils.WaitUntil.elementToBeVisible(WaitUntil.java:20)\r\n\tat PageObjects.HomePage.HomePage.getSearchResultTitle(HomePage.java:39)\r\n\tat stepdefinitions.ShouldBeAbleToSearchProductStepDefs.i_should_be_able_to_view_all_the_relevant_products(ShouldBeAbleToSearchProductStepDefs.java:33)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:26)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:20)\r\n\tat cucumber.runtime.java.JavaStepDefinition.execute(JavaStepDefinition.java:57)\r\n\tat cucumber.runner.PickleStepDefinitionMatch.runStep(PickleStepDefinitionMatch.java:50)\r\n\tat cucumber.runner.TestStep.executeStep(TestStep.java:65)\r\n\tat cucumber.runner.TestStep.run(TestStep.java:50)\r\n\tat cucumber.runner.PickleStepTestStep.run(PickleStepTestStep.java:43)\r\n\tat cucumber.runner.TestCase.run(TestCase.java:46)\r\n\tat cucumber.runner.Runner.runPickle(Runner.java:50)\r\n\tat io.cucumber.junit.PickleRunners$NoStepDescriptions.run(PickleRunners.java:146)\r\n\tat io.cucumber.junit.FeatureRunner.runChild(FeatureRunner.java:68)\r\n\tat io.cucumber.junit.FeatureRunner.runChild(FeatureRunner.java:23)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat io.cucumber.junit.Cucumber.runChild(Cucumber.java:144)\r\n\tat io.cucumber.junit.Cucumber.runChild(Cucumber.java:65)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat io.cucumber.junit.Cucumber$RunCucumber.evaluate(Cucumber.java:174)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat org.apache.maven.surefire.junit4.JUnit4Provider.execute(JUnit4Provider.java:369)\r\n\tat org.apache.maven.surefire.junit4.JUnit4Provider.executeWithRerun(JUnit4Provider.java:275)\r\n\tat org.apache.maven.surefire.junit4.JUnit4Provider.executeTestSet(JUnit4Provider.java:239)\r\n\tat org.apache.maven.surefire.junit4.JUnit4Provider.invoke(JUnit4Provider.java:160)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.invokeProviderInSameClassLoader(ForkedBooter.java:373)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.runSuitesInProcess(ForkedBooter.java:334)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.execute(ForkedBooter.java:119)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.main(ForkedBooter.java:407)\r\n",
+  "status": "failed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
